e201f4dc30de391587c3a5c9e169fc4fe47447d4
Jose2300 <joitalomaciel@gmail.com>
2023-03-19 21:50:00 -0300

    commit

---
+++ file.js
@@ -0,0 +1,37 @@
+function mudarDisplay() {
+        const minhaDiv = document.getElementById("card");
+        if (minhaDiv.style.display === "none") {
+          minhaDiv.style.display = "block";
+        } else {
+          minhaDiv.style.display = "none";
+        }
+      }
+
+      var minhaDiv = document.getElementById("card");
+      var posicaoInicial = {x: 0, y: 0};
+      var posicaoAtual = {x: 0, y: 0};
+      var mousePressionado = false;
+      
+      minhaDiv.addEventListener("mousedown", function(e) {
+        posicaoInicial.x = e.clientX;
+        posicaoInicial.y = e.clientY;
+        mousePressionado = true;
+      });
+      
+      document.addEventListener("mousemove", function(e) {
+        if (mousePressionado) {
+          posicaoAtual.x = e.clientX;
+          posicaoAtual.y = e.clientY;
+          var deltaX = posicaoAtual.x - posicaoInicial.x;
+          var deltaY = posicaoAtual.y - posicaoInicial.y;
+          minhaDiv.style.left = (minhaDiv.offsetLeft + deltaX) + "px";
+          minhaDiv.style.top = (minhaDiv.offsetTop + deltaY) + "px";
+          posicaoInicial.x = posicaoAtual.x;
+          posicaoInicial.y = posicaoAtual.y;
+        }
+      });
+      
+      document.addEventListener("mouseup", function(e) {
+        mousePressionado = false;
+      });
+      
