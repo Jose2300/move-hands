38f9b0009109d790b1d3518f1646ac843ff9f981
Jose2300 <joitalomaciel@gmail.com>
2023-05-24 01:08:14 -0300

    commit

---
+++ Componentes/terra/terra.js
@@ -0,0 +1,21 @@
+const miDiv = document.getElementById("terra");
+const minhaDiv = document.getElementById("lua");
+
+function mudarDisplay() { 
+        
+        if (minhaDiv.style.display === "none") {
+          minhaDiv.style.display = "block";
+          miDiv.style.display = "none";
+        } else {
+          minhaDiv.style.display = "none";
+        }
+      }
+      function mudarDispla() {
+        if (miDiv.style.display === "none") {
+          miDiv.style.display = "block";
+          minhaDiv.style.display = "none";
+        } else {
+          miDiv.style.display = "none";
+        }
+      }
+    
