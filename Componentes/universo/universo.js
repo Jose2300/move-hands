f992da44fec4594f8118ddacb464f2a975c47b31
Jose2300 <63307537+Jose2300@users.noreply.github.com>
2023-05-25 14:02:34 -0300

    Update universo.js

--- Componentes/universo/universo.js
+++ Componentes/universo/universo.js
@@ -1,40 +1,173 @@
-const planets = document.querySelectorAll('.planet')
-const p_radii = [22,33,50,70,112,138,165,190]
-let p_radians = new Array(8).fill(0)
-const p_velocities = [1.607, 1.174,1,0.802, 0.434, 0.323, 0.228, 0.182]
+const sun = document.getElementById("sun");
+const mercurio = document.getElementById("mercurio");
+const venus = document.getElementById("venus");
+const terra = document.getElementById("earth");
+const marte = document.getElementById("marte");
+const jupter = document.getElementById("jupter");
+const saturno = document.getElementById("saturno");
+const  urano = document.getElementById("urano");
+const netuno = document.getElementById("netuno");
+const plutao = document.getElementById("plutao");
 
-const moon = document.querySelector('#moon')
-const m_radius = 8
-let m_radians = 0
-const m_velocity = 10
-
-const p_orbits = document.querySelectorAll('.p-orbit')
-const m_orbit = document.querySelector('#m-orbit')
-
-p_orbits.forEach((p_orbit, index)=>{
-  p_orbit.style.height = `${p_radii[index]}vmin`
-  p_orbit.style.width = `${p_radii[index]}vmin`
-})
-
-setInterval( ()=> {
-  planets.forEach( (planet, index)=>{
-    planet.style.left = `${Math.cos(p_radians[index]) * p_radii[index]}vmin`
-    planet.style.top = `${Math.sin(p_radians[index]) * p_radii[index]}vmin`
-    p_radians[index] += p_velocities[index] * 0.02
-  })
-
-  moon.style.left = `${earthX() + (Math.cos(m_radians) * m_radius )}vmin`
-  moon.style.top = `${earthY() + (Math.sin(m_radians) * m_radius )}vmin`
-  m_radians += m_velocity * 0.02
-
-  m_orbit.style.left = `${earthX()}vmin`
-  m_orbit.style.top = `${earthY()}vmin`
-}, 1000/60)
-
-function earthX(){
-  return Number( planets[2].style.left.split('vmin')[0] )
-}
-
-function earthY(){
-  return Number( planets[2].style.top.split('vmin')[0] )
-}
+function mudarSol() { 
+        
+        if (sun.style.display === "none") {
+          sun.style.display = "block";
+          mercurio.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          sun.style.display = "none";
+        }
+      }
+      function mudarMercurio() {
+        if (mercurio.style.display === "none") {
+          mercurio.style.display = "block";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          mercurio.style.display = "none";
+        }
+      }
+      function mudarVenus() {
+        if (venus.style.display === "none") {
+          venus.style.display = "block";
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          venus.style.display = "none";
+        }
+      }
+      function mudarTerra() {
+        if (terra.style.display === "none") {
+          terra.style.display = "block";
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          terra.style.display = "none";
+        }
+      }
+      function mudarMarte() {
+        if (marte.style.display === "none") {
+          marte.style.display = "block";
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          marte.style.display = "none";
+        }
+      }
+      function mudarJupter() {
+        if (jupter.style.display === "none") {
+          jupter.style.display = "block";
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          jupter.style.display = "none";
+        }
+      }
+      function mudarSaturno() {
+        if (saturno.style.display === "none") {
+          saturno.style.display = "block";
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          saturno.style.display = "none";
+        }
+      }
+      function mudarUrano() {
+        if (urano.style.display === "none") {
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "block";
+          netuno.style.display = "none";
+          plutao.style.display = "none";
+        } else {
+          urano.style.display = "none";
+        }
+      }
+      function mudarNetuno() {
+        if (netuno.style.display === "none") {
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "block";
+          plutao.style.display = "none";
+        } else {
+          netuno.style.display = "none";
+        }
+      }
+      function mudarPlutao() {
+        if (plutao.style.display === "none") {
+          mercurio.style.display = "none";
+          sun.style.display = "none";
+          venus.style.display = "none";
+          terra.style.display = "none";
+          marte.style.display = "none";
+          jupter.style.display = "none";
+          saturno.style.display = "none";
+          urano.style.display = "none";
+          netuno.style.display = "none";
+          plutao.style.display = "block";
+        } else {
+          plutao.style.display = "none";
+        }
+      }
+      
